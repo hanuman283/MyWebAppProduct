4a72bd426503b9d05705a11dcf2424df0d61deab
hanuman283 <hanumant.shinde283@gmail.com>
2025-11-01 21:48:07 +0530

    allow origin

--- MyWebAppProduct/Program.cs
+++ MyWebAppProduct/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp",
-        policy => policy.WithOrigins("http://localhost:4200")
+        policy => policy.WithOrigins("*")
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
